169b389acc1eb3c1de74c7859b0b2a0e82ac1576
Richard Venneman <richardvenneman@me.com>
2022-11-27 19:36:39 +0100

    Add form input

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   return (
     <div className="flex flex-wrap h-screen">
       <main className="md:flex-auto"></main>
-      <aside className="bg-rise-grayscale-white flex-initial w-full md:w-80">
-        <p className="font-bold px-4 py-6 text-xl">Emiel</p>
+      <aside className="bg-rise-grayscale-white flex-initial px-4 py-6 w-full md:w-80">
+        <form>
+          <input type="text" className="font-bold text-xl w-full placeholder:text-rise-grayscale-400 focus:outline-none" placeholder="Event name" />
+        </form>
       </aside>
     </div>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,7 +21,7 @@ module.exports = {
     },
     fontFamily: {
       sans: ['Inter', ...defaultTheme.fontFamily.sans]
-    }
+    },
   },
   plugins: [],
 }
