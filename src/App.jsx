952efdb66a57f612aa875153666374a43c2befaf
Richard Venneman <richardvenneman@me.com>
2022-11-27 19:07:16 +0100

    Add layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <main></main>
-      <aside className="bg-rise-grayscale-white">
+    <div className="flex flex-wrap h-screen">
+      <main className="md:flex-auto"></main>
+      <aside className="bg-rise-grayscale-white flex-initial w-full md:w-80">
         <p className="font-bold px-4 py-6 text-xl">Emiel</p>
       </aside>
     </div>
