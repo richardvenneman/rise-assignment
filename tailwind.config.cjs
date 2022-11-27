c3bbca7b5994d4b79fec091a9f1bf406586412f9
Richard Venneman <richardvenneman@me.com>
2022-11-27 17:10:43 +0000

    Add Tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'rise': {
+        'grayscale': {
+          white: '#FFFFFF',
+          gray30: '#F7F7FC',
+          gray400: '#BDBFC5',
+          gray600: '#848793',
+        },
+        positive: '#19BE2F',
+        primary: '#7161EC',
+      },
+    },
   },
   plugins: [],
 }
