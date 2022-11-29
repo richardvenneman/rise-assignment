0d2afbb4aae8032e478f2e0ceb26bb6770faee16
Richard Venneman <richardvenneman@me.com>
2022-11-29 12:29:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rise-assignment/',
+  base: './',
   plugins: [
     eslintPlugin(),
     react(),
